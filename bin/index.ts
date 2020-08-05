720bcb3ab2536e6f146c9a2d4ad1414ab757f63a
zephyru <zephyru@163.com>
2020-08-05 14:09:36 +0800

    refresh

--- bin/index.ts
+++ bin/index.ts
@@ -1,48 +1,41 @@
-
-import app from '../lib/koa-server'
-import * as http from "http"
-import {port} from '../config/'
-import {loggerRes} from '../lib/utils/logger'
-const debug = require('debug')('demo:server')
+import app from "../lib/koa-server";
+import * as http from "http";
+import { port, host } from "../config/";
+import { loggerRes, logger } from "../lib/utils/logger";
+const debug = require("debug")("demo:server");
 // 打印输出端口号
-loggerRes.info('listen prot: ' + port);
+logger.info(`http listen: ${host}:${port}`);
 const server = http.createServer(app.callback());
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(port, host);
+server.on("error", onError);
+server.on("listening", onListening);
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+    if (error.syscall !== "listen") {
+        throw error;
+    }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-  switch (error.code) {
-    case 'EACCES':
-    loggerRes.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-    loggerRes.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+    switch (error.code) {
+        case "EACCES":
+            loggerRes.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            loggerRes.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+    var addr = server.address();
+    var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    debug("Listening on " + bind);
 }
 
-export default server
-export {
-  server,
-}
+export default server;
+export { server };
--- config/index.ts
+++ config/index.ts
@@ -8,14 +8,22 @@
  * log.level = watching 输出日志在控制台将必要日志写入日志文件
  * log.level = running 只将必要的日志写入日志文件
  */
-var config = {
-    host: "localhost",
+const Base = {
+    host: "0.0.0.0",
     port: 8082,
+    dbAddress: "./db/production.db",
     logConfig: {
         basePath: "../logs",
-        level: "debug", // debug watching  running
+        level: "watching", // debug watching  running
     },
 };
-export let { logConfig, port } = config;
+export const viewDir = "../view";
 
-export default config;
+//开发模式的配置
+if (process.env.NODE_ENV === "development") {
+    Base.logConfig.level = "debug";
+    Base.dbAddress.replace("production", "development");
+}
+
+export const { port, host, logConfig, dbAddress } = Base;
+export default Base;
