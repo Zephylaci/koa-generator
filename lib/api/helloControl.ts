f7ddbf975839b622a3e92e4d2f577750e37081fd
zephyru <zephyru@163.com>
2020-08-05 15:18:21 +0800

    refresh

--- lib/api/helloControl.ts
+++ lib/api/helloControl.ts
@@ -1,18 +1,7 @@
 import * as router from "koa-router";
-import { loggerErr } from "../utils/logger";
-import { createErrorMessage } from "../utils/utils";
 const HelloHandle = new router();
 HelloHandle.get("/hello", (ctx, next) => {
     ctx.body = "Hello World!";
-
-    const ErrorMessage = new Error(
-        createErrorMessage({
-            from: "Hello",
-            type: "O",
-            text: "Hello failed!",
-        })
-    );
-    loggerErr.error(ErrorMessage);
 });
 
 export default HelloHandle;
